eebe048ccd7fea8d3e32ac1155f2fbf2761e5b9f
Matheus <matheusmartinscia@gmail.com>
2024-07-24 22:32:05 -0300

    Classes Entidade Implementadas

--- src/java/entities/Department.java
+++ src/java/entities/Department.java
@@ -1,5 +1,59 @@
 package java.entities;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
 
-public class Department {
+public class Department implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private Map<Integer, Seller> sellers = new TreeMap<>();
+	
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+	public Map<Integer, Seller> getSellers() {
+		return sellers;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nID: " + this.getId());
+		sb.append("\nNAME: " + this.getName());
+		return sb.toString();
+	}
+	
+	
+	
 }
--- src/java/entities/Seller.java
+++ src/java/entities/Seller.java
@@ -1,5 +1,80 @@
 package java.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
 
+public class Seller implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name, email;
+	private Date birthDate;
+	private Double baseSalary;
+	private Department department;
+	
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nID: " + this.getId());
+		sb.append("\nNAME: " + this.getName());
+		sb.append("\nEMAIL: " + this.getEmail());
+		sb.append("\nBIRTH DATE: " + this.getBirthDate());
+		sb.append("\nBASE SALARY: " + this.getBaseSalary());
+		sb.append("\nDEPARTMENT ID: " + this.getDepartment().getId());
+		sb.append("\nDEPARTMENT NAME: " + this.getDepartment().getName());
+		return sb.toString();
+	}
+	
+	
 }
